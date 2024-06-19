5de3061d70e61b47d0f0c1784cf74933c09bfc86
DiadNetworks <programming@diadnet.com>
2024-06-19 10:30:04 -0400

    Update MainGUI.ps1

--- MainGUI.ps1
+++ MainGUI.ps1
@@ -289,7 +289,7 @@ function Add-AutoReply {
 	# internalReplyLabel
 	#
 	$internalReplyLabel.AutoSize = $true
-	$internalReplyLabel.Location = New-Object System.Drawing.Point(8, 50)
+	$internalReplyLabel.Location = New-Object System.Drawing.Point(8, 56)
 	$internalReplyLabel.Name = "internalReplyLabel"
 	$internalReplyLabel.Size = New-Object System.Drawing.Size(100, 13)
 	$internalReplyLabel.TabIndex = 2
@@ -320,7 +320,7 @@ function Add-AutoReply {
 	# externalReplyLabel
 	#
 	$externalReplyLabel.AutoSize = $true
-	$externalReplyLabel.Location = New-Object System.Drawing.Point(218, 50)
+	$externalReplyLabel.Location = New-Object System.Drawing.Point(218, 56)
 	$externalReplyLabel.Name = "externalReplyLabel"
 	$externalReplyLabel.Size = New-Object System.Drawing.Size(103, 13)
 	$externalReplyLabel.TabIndex = 4
